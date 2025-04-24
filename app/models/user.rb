ee52468e8d99d40c44626791ca5f18c7cfda320d
Leslie Williamson <leslie.williamson@live.ca>
2025-04-24 17:19:18 -0400

    Show user name in the navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   # :registerable, :recoverable, :rememberable,
   devise :database_authenticatable, :validatable
-  
+
   belongs_to :company
+
+  def name
+    email.split("@").first.capitalize
+  end
 end
